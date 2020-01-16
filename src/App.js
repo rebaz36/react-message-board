b51bee2acdeb63cd3c77e8efe529474ea907dbec
Shaunbus <sjbucyk@live.ca>
2020-01-16 19:25:45 +0000

    working header, board, and footer unstyled

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import MessageBoard from './MessageBoard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+var messageBrd = [{id : 1, name : "Bill", message : "Hi All!"},
+                  {id : 2, name : "Ann", message : "ICS 221 is fun!"},
+                  {id : 3, name : "Johnny", message : "I'm stranded!"},
+                  {id : 4, name : "Barb", message : "Hi"},
+                  {id : 5, name : "Frank", message : "Who's tired?"},
+                  {id : 6, name : "Sarah", message : "I heart React"}]
+
+export const App = ({className}) => (
+  <div className={className}>
+    <Header />
+    <MessageBoard messageBrd = {messageBrd}/>
+    <Footer />
+  </div>
+)
 
 export default App;
